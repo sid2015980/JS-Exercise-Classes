835a5ad185465492ed1b56d2e806e891340e98be
sid2015980 <sidneybroadnax@gmail.com>
2020-04-30 14:09:34 -0700

    Finished

--- index.js
+++ index.js
@@ -42,6 +42,25 @@ class Airplane {
 
 class Person {
 
+  constructor(name, age) {
+    this.stomach = [];
+    this.name = name;
+    this.age = age;
+    }//The methods for Person will be all here instead of outside the class
+      eat(someFood) {
+        if (this.stomach.length < 10) {
+        return this.stomach.push(someFood);
+       }
+      }
+    
+      poop(){
+        return(this.stomach = []);
+      }
+    
+      toString(){
+        return `${this.name}, ${this.age}`;
+      }
+
 }
 
 /*
@@ -59,6 +78,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.tank = 0;
+    this.odometer = 0;
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;  
+    }
+  
+    fill(gallons) {
+    return (this.tank += gallons);
+    }
+  
+    drive(distance) {
+      const maxDistance = this.tank * this.milesPerGallon;
+      if (distance <= maxDistance) {
+        this.odometer += distance;
+        this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
 
 }
 
@@ -75,8 +116,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
-}
+  constructor(attributes){
+  this.name = attributes.name,
+  this.age = attributes.age,
+  this.location = attributes.location
+    }
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+  }
 
 /*
   TASK 4
@@ -92,8 +140,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attributes){
+  super(attributes)
+  this.specialty = attributes.specialty;
+  this.favLanguage = attributes.favLanguage;
+  this.catchPhrase = attributes.catchPhrase;
+  }
+demo(subject){
+  return `Today we are learning about ${'redux'}`
+}
+grade(student, subject){
+  return `${student.name} receives a perfect score on ${subject}`
+  }
 }
 
 /*
@@ -111,9 +170,27 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes){
+    super(attributes)
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
 
+  listSubjects(favSubjects){
+    return this.favSubjects
+  }
+  PRAssignment(subject){
+  return `${this.name} has submitted a PR for ${subject}`;
 }
+
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+}
+}
+
+// console.log(this.name());
 
 /*
   TASK 6
@@ -128,7 +205,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor(attributes){
+  super(attributes)  
+  this.gradClassName = attributes.gradClassName;
+  this.favInstructor = attributes.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode(student){
+    return `${this.name} debugs ${student.name}'s code on ${'redux'}`
+  }
 
 }
 
